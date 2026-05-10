6c3f072c6ea4b6c532955f225a1aff586d52d498
thanhibis <thanh@ibis.studio>
2026-05-10 16:59:45 +0700

    up

---
+++ js/setting.js
@@ -0,0 +1,37 @@
+//menu header
+$('.js-mobile').on('click', function(){
+  $(this).toggleClass("js-mobile--close");
+  $("html").toggleClass("js-locked");
+  // $(".nav-menu").slideToggle();
+  // e.preventDefault();
+  $(".header-nav").fadeToggle();
+});
+
+
+$('.float-right__menu-option').on('click', function(){
+  $(this).toggleClass("active");
+  $(".float-right").toggleClass("active");
+});
+
+
+
+
+
+
+//backtop
+jQuery(document).ready(function ($) {
+  $(window).on("scroll", function () {
+    if($(window).scrollTop()> $("#footer").offset().top - $(window).outerHeight()){
+      $(".float-right").addClass("fixd");
+    }
+    else {
+      $(".float-right").removeClass("fixd");
+    }
+  });
+  $('.js-backtop').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 500);
+    return false;
+  });
+});
